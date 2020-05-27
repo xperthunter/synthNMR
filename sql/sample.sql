653a9c41376b57c022651c066cea57eb0facc334
Амалия <Amalia@Amalia-Macbook.local>
2020-05-27 16:31:48 -0700

    changes to synth_tester: img_specs table and plot_specs table

--- sql/sample.sql
+++ sql/sample.sql
@@ -7,21 +7,24 @@ CREATE TABLE data (
 	centers_pixels BLOB,
 	todays_date TEXT,
 	img_specs_id INTEGER,
-	FOREIGN KEY ([img_specs_id]) REFERENCES "img_specs" ([id]) ON DELETE NO
-	ACTION ON UPDATE NO ACTION
+	plot_specs_id INTEGER,
+	FOREIGN KEY ([img_specs_id]) REFERENCES "img_specs" ([id]) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY ([plot_specs_id]) REFERENCES "plot_specs" ([id]) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
 CREATE TABLE img_specs (
-    id INTEGER PRIMARY KEY NOT NULL,
-    figsize TEXT,
-    dpi TEXT
+	id INTEGER PRIMARY KEY NOT NULL,
+	figsize TEXT,
+	dpi TEXT
 );
 
 CREATE TABLE plot_specs (
-    id INTEGER PRIMARY KEY NOT NULL,
-    density FLOAT,
-    lower_limit FLOAT,
-    upper_limit FLOAT,
-    mode TEXT,
-    variance FLOAT
+	id INTEGER PRIMARY KEY NOT NULL,
+	density FLOAT,
+	lower_limit FLOAT,
+	upper_limit FLOAT,
+	mode TEXT,
+	variance FLOAT
 );
+
+
--- tools/synth_tester.py
+++ tools/synth_tester.py
@@ -17,6 +17,9 @@ parser.add_argument('-p', required=True, type=int,
 	metavar='value', help='Number of points for the peak generator')
 parser.add_argument('-s', required=True, type=str,
 	metavar='schema', help='Path to schema')
+parser.add_argument('-m', required=False, type=str,
+	metavar='<str>', help='Mode for placing peaks in spectrum')
+
 arg = parser.parse_args()
 
 conn = sqlite3.connect(arg.d)
@@ -27,6 +30,7 @@ c = conn.cursor()
 try:
 	c.execute(f"DROP TABLE data")
 	c.execute(f"DROP TABLE img_specs")
+	c.execute(f"DROP TABLE plot_specs")
 	with open(arg.s) as fp:
 		c.executescript(fp.read())
 except:
@@ -34,7 +38,7 @@ except:
 		c.executescript(fp.read())
 
 sql = ''.join((f'INSERT OR IGNORE INTO data (points, np_plot, center_coords, ',
-			   'plot_pixels, centers_pixels, todays_date, img_specs_id) VALUES(?, ?, ?, ?, ?, ?, ?)'))
+			   'plot_pixels, centers_pixels, todays_date, img_specs_id, plot_specs_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?)'))
 print(sql)
 xyz, mm, vv = gen.peak_generator(arg.p, mode="uniform")
 img, peaks = gen.spectra_generator(xyz, mm)
@@ -45,24 +49,34 @@ ax.contourf(xyz[:,:,0],xyz[:,:,1],xyz[:,:,2], 100, cmap="gray_r")
 plt.axis('off')
 plt.show()
 
-###my changes
-img_sql = ((f'INSERT INTO img_specs (figsize, dpi) VALUES (?, ?)')) ###function
-inserter_img = ['3in', '100']
-c.execute(img_sql, inserter_img)
+def img_specs_inserter(cursor):
+	img_sql = ((f'INSERT INTO img_specs (figsize, dpi) VALUES (?, ?)')) ###function
+	inserter_img = ['3in', '100']
+	cursor.execute(img_sql, inserter_img)
+#	return conn
 
-img_spec_q = c.execute(f"SELECT id FROM img_specs WHERE figsize = '3in' AND dpi = '100'") ###function
-img_specs_id = c.fetchone()[0]
-print(img_specs_id)
-#is it correct to use the same name as in the table?
-#do we use fetchone, because for now we only do 1 entry into the table?
+def img_specs_query(cursor):
+	img_spec_q = cursor.execute(f"SELECT id FROM img_specs WHERE figsize = '3in' AND dpi = '100'") ###function
+	img_specs_id = cursor.fetchone()[0]
+	return img_specs_id
+#print(img_specs_id)
+
+plot_sql = ''.join((f'INSERT INTO plot_specs (density, lower_limit, upper_limit, mode, ',
+				'variance) VALUES (?, ?, ?, ?, ?)'))
+inserter_plot = [1000.0, 0.0, 10.0, 'default', 1.0]
+c.execute(plot_sql, inserter_plot)
+
+plot_specs_q = c.execute(f"SELECT id FROM plot_specs WHERE density = 1000.0 AND lower_limit = 0.0 AND upper_limit = 10.0 AND mode = 'default' AND variance = 1.0 ")
+plot_specs_id = c.fetchone()[0]
+
+img_specs_inserter(c)
 
 
-inserter = [arg.p, xyz.tobytes(), mm.tobytes(), img.tobytes(), peaks.tobytes(), '01/01/01', img_specs_id]
+inserter = [arg.p, xyz.tobytes(), mm.tobytes(), img.tobytes(), peaks.tobytes(), '01/01/01', img_specs_query(c), plot_specs_id]
 
 c.execute(sql, inserter)
 #executing the inserter to insert information into the data,
 #So we need sql (the command that inserts into the table "data", and we also need the inserter itself
-#why do we split values and the inserter?
 
 #executor for the img_specs
 
