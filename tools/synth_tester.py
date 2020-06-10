b214ddd494c7f42faf6841fa6f7b9852112a5688
Amalia <Amalia@Amalia-Macbook.local>
2020-06-10 11:30:38 -0700

    updated synth_tester.py

--- tools/synth_tester.py
+++ tools/synth_tester.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import datetime
 import sys
 import argparse
 import sqlite3
@@ -17,29 +18,29 @@ parser.add_argument('-p', required=True, type=int,
 	metavar='value', help='Number of points for the peak generator')
 parser.add_argument('-s', required=True, type=str,
 	metavar='schema', help='Path to schema')
-parser.add_argument('-m', required=False, type=str,
-	metavar='<str>', help='Mode for placing peaks in spectrum')
+#parser.add_argument('-m', required=False, type=str,
+	#metavar='<str>', help='Mode for placing peaks in spectrum')
 
 arg = parser.parse_args()
-
+#print(datetime.date.today())
 conn = sqlite3.connect(arg.d)
-#PRAGMA
-#conn.execute("PRAGMA foreign_keys")
+
 c = conn.cursor()
 #fixed dropping table
 try:
-	c.execute(f"DROP TABLE data")
-	c.execute(f"DROP TABLE img_specs")
-	c.execute(f"DROP TABLE plot_specs")
-	c.execute(f"DROP TABLE user_info")
+	c.execute("DROP TABLE data")
+	c.execute("DROP TABLE img_specs")
+	c.execute("DROP TABLE plot_specs")
+	c.execute("DROP TABLE user_info")
 	with open(arg.s) as fp:
 		c.executescript(fp.read())
 except:
 	with open(arg.s) as fp:
 		c.executescript(fp.read())
 
-sql = ''.join((f'INSERT OR IGNORE INTO data (points, np_plot, center_coords, ',
-			   'plot_pixels, centers_pixels, todays_date, img_specs_id, plot_specs_id, user_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)'))
+sql = ''.join(('INSERT OR IGNORE INTO data (points, np_plot, center_coords, ',
+			   'plot_pixels, centers_pixels, todays_date, img_specs_id, plot_specs_id,',
+			   ' user_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)'))
 print(sql)
 xyz, mm, vv = gen.peak_generator(arg.p, mode="uniform")
 img, peaks = gen.spectra_generator(xyz, mm)
@@ -51,47 +52,70 @@ plt.axis('off')
 plt.show()
 
 #img_specs table
-def img_specs_inserter(cursor):
+def img_specs_inserter(cursor, inserter_img1):
 	img_sql = ((f'INSERT INTO img_specs (figsize, dpi) VALUES (?, ?)')) ###function
-	inserter_img = ['3in', '100']
-	cursor.execute(img_sql, inserter_img)
+	#inserter_img = ['3in', '100']
+	cursor.execute(img_sql, inserter_img1)
 #connecting to the table data; foreign key = img_specs_id
-def img_specs_query(cursor):
-	img_spec_q = cursor.execute(f"SELECT id FROM img_specs WHERE figsize = '3in' AND dpi = '100'")
+def img_specs_query(cursor, query_img): ###arguments
+	img_spec_q = cursor.execute(''.join((f"SELECT id FROM img_specs WHERE figsize = '{query_img[0]}'",
+		    							 f" AND dpi = '{query_img[1]}'")))
 	img_specs_id = cursor.fetchone()[0]
-	return img_specs_id
+	args = locals().copy()
+	return img_specs_id, args
 #print(img_specs_id)
 
 #plot_specs table
-def plot_specs_inserter(cursor):
+def plot_specs_inserter(cursor, inserter_plot):
 	plot_sql = ''.join((f'INSERT INTO plot_specs (density, lower_limit, upper_limit, mode, ',
 					'variance) VALUES (?, ?, ?, ?, ?)'))
-	inserter_plot = [1000.0, 0.0, 10.0, 'default', 1.0]
+	#inserter_plot = [1000.0, 0.0, 10.0, 'default', 1.0]
 	cursor.execute(plot_sql, inserter_plot)
 
-def plot_specs_query(cursor):
-	plot_specs_q = cursor.execute(f"SELECT id FROM plot_specs WHERE density = 1000.0 AND lower_limit = 0.0 AND upper_limit = 10.0 AND mode = 'default' AND variance = 1.0 ")
+def plot_specs_query(cursor, query_plot):
+	plot_specs_q = cursor.execute(''.join((f"SELECT id FROM plot_specs WHERE density = '{query_plot[0]}' AND",
+										   f" lower_limit = '{query_plot[1]}' AND upper_limit = '{query_plot[2]}'",
+										   f" AND mode = '{query_plot[3]}' AND variance = '{query_plot[4]}'")))
 	plot_specs_id = cursor.fetchone()[0]
-	return plot_specs_id
+	args = locals().copy()
+	return plot_specs_id, args
 
 #user_info table
-def user_info_inserter(cursor):
+def user_info_inserter(cursor, inserter_user):
 	user_sql = ((f'INSERT INTO user_info (first_name, last_name, email) VALUES (?, ?, ?)'))
-	inserter_user = ['Stephani', 'Keefe', 'skeefe@ucdavis.edu']
+	#inserter_user = ['Stephani', 'Keefe', 'skeefe@ucdavis.edu']
 	cursor.execute(user_sql, inserter_user)
 
-def user_info_query(cursor):
-	user_specs_q = cursor.execute(f"SELECT id FROM user_info WHERE first_name = 'Stephani' AND last_name = 'Keefe' AND email = 'skeefe@ucdavis.edu'")
+def user_info_query(cursor, query_user):
+	user_specs_q = cursor.execute(''.join((f"SELECT id FROM user_info WHERE first_name = '{query_user[0]}'",
+										   f" AND last_name = '{query_user[1]}' AND email = '{query_user[2]}'")))
 	user_id = cursor.fetchone()[0]
-	return user_id
+	args = locals().copy()
+	return user_id, args
 
 
-img_specs_inserter(c)
-plot_specs_inserter(c)
-user_info_inserter(c)
+img_specs_inserter(c, ['3in', '100'])
+x,a = img_specs_query(c,['3in', '100'])
 
-inserter = [arg.p, xyz.tobytes(), mm.tobytes(), img.tobytes(), peaks.tobytes(), '01/01/01', img_specs_query(c), plot_specs_query(c), user_info_query(c)]
+plot_specs_inserter(c, [1000.0, 0.0, 10.0, 'default', 1.0])
+a,b = plot_specs_query(c,[1000.0, 0.0, 10.0, 'default', 1.0])
 
+user_info_inserter(c, ['Stephani', 'Keefe', 'skeefe@ucdavis.edu'])
+d,e = user_info_query(c,['Stephani', 'Keefe', 'skeefe@ucdavis.edu'])
+
+#print(a)
+#sys.exit()
+
+inserter = [arg.p,
+			xyz.tobytes(),
+			mm.tobytes(),
+			img.tobytes(),
+			peaks.tobytes(),
+			datetime.date.today(),
+			img_specs_query(c, ['3in', '100'])[0],
+			plot_specs_query(c,[1000.0, 0.0, 10.0, 'default', 1.0])[0],
+			user_info_query(c,['Stephani', 'Keefe', 'skeefe@ucdavis.edu'])[0]]
+#print(type(inserter[6]))
 c.execute(sql, inserter)
 #executing the inserter to insert information into the data,
 #So we need sql (the command that inserts into the table "data", and we also need the inserter itself
